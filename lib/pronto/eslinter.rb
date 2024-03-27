78ecb5a45186e2d05538f9d8859b5982ef02fb1b
Ugnius Pacauskas                                                                                                                                                                                                                                                          Ugnius Pacauskas <ugnius.pacauskas@vinted.com>
2024-03-27 14:34:44 +0200

    Added commit sha

--- lib/pronto/eslinter.rb
+++ lib/pronto/eslinter.rb
@@ -13,18 +13,19 @@ module Pronto
         .select { |patch| patch.additions.positive? }
         .flat_map { |patch| patch.new_file_full_path.to_s}
         .select{ |file| file[/\.js$|\.jsx$|\.ts$|\.tsx$/] }
+      commit_sha = @patches.commit
 
       return [] unless files
       eslint_output = Pronto::Eslinter::Eslint.new(files).lint
       suggestions = Pronto::Eslinter::Suggestions.new(eslint_output).suggest
 
-      messages(suggestions)
+      messages(suggestions, commit_sha)
     end
 
-    def messages(suggestions)
+    def messages(suggestions, commit_sha)
       messages = []
       suggestions.map do |suggestion|
-        Message.new(suggestion[:path], suggestion[:line], :warning, msg(suggestion), nil, self.class)
+        Message.new(suggestion[:path], suggestion[:line], :warning, msg(suggestion), commit_sha, self.class)
       end
       messages
     end
@@ -32,7 +33,7 @@ module Pronto
     def msg(suggestion)
       return suggestion[:text] unless suggestions? && suggestion[:suggestions]
 
-      "#{suggestion[:text]}\n\n```suggestion\n#{suggestion[:suggestion]}```"
+      "#{suggestion[:text]}\n\n```suggestion\n#{suggestion[:suggestions]}```"
     end
 
     def suggestions?
